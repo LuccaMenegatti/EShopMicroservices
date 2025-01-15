949de2b957abeee0c7e6ed6a5f70f257f54f937c
Lucca Menegatti <luccamenegatti@overdriveconsultoria.com.br>
2025-01-15 01:29:16 -0300

    connect database catalogDb in docker-compose

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -6,7 +6,7 @@ namespace Catalog.API.Products.CreateProduct;
 public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken ct)
     {
@@ -19,8 +19,9 @@ internal class CreateProductCommandHandler : ICommandHandler<CreateProductComman
             Price = command.Price
         };
 
-        // Save product to database
+        session.Store(product);
+        await session.SaveChangesAsync(ct);
 
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
     }
 }
