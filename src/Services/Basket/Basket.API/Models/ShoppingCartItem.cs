4ce2108c7fbd342d772ab955636dceed0012bf73
Lucca Menegatti <menegatti.luccaantonio@gmail.com>
2025-01-29 00:40:06 -0300

    created shopping carter model and basket endpoints

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCartItem
+{
+    public int Quantity { get; set; } = default!;
+    public string Color { get; set; } = string.Empty;
+    public decimal Price { get; set; } = decimal.Zero;
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+}
